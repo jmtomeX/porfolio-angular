9726b988842502f639a5eb23b54fccf0b27ba2e6
Jose Mari <iremti2@gmail.com>
2020-11-19 13:18:50 +0100

    Terminado animaciones

--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild  } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProyectosService } from '../../services/proyectos.service';
 @Component({
@@ -7,16 +7,22 @@ import { ProyectosService } from '../../services/proyectos.service';
   styleUrls: ['./portafolio.component.css']
 })
 export class PortafolioComponent implements OnInit {
-
+  @ViewChild('divLogo')divLogo: ElementRef;
   constructor(
     public projectService: ProyectosService,
-    private router: Router) { }
+    private router: Router,
+    private renderer: Renderer2) {
+     }
 
   ngOnInit(): void {
 
   }
   search(logo: string) {
-    this.router.navigate(['./search/' + logo]);
-  }
+    // this.renderer.removeClass(this.divLogo.nativeElement, '');
+    this.renderer.addClass(this.divLogo.nativeElement, 'enter-search');
 
+    setTimeout(() => {
+      this.router.navigate(['./search/' + logo]);
+    }, 1500);
+  }
 }
